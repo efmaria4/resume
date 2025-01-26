173f65707c62855d654ebe15456f2f038c3ca454
Maria Efstratiou <efmaria944@gmail.com>
2025-01-26 10:35:57 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Event Listeners for Flags
+document.getElementById("english-flag").addEventListener("click", () => {
+    window.location.href = "index-en.html"; // Navigate to the English version
+  });
+  
+  document.getElementById("greek-flag").addEventListener("click", () => {
+    window.location.href = "index-gr.html"; // Navigate to the Greek version
+  });
+  
