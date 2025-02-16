d3464cc1f9b7e9a67d1f97ca0bc9cf77c69fb867
Maria Efstratiou <efmaria944@gmail.com>
2025-02-16 23:12:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ document.getElementById("english-flag").addEventListener("click", () => {
   document.getElementById("greek-flag").addEventListener("click", () => {
     window.location.href = "https://github.com/efmaria4/resume/blob/main/greek.html"; // Navigate to the Greek version
   });
+
+<button onclick="window.open('https://yourwebsite.com', '_blank')">Websites</button>
+<button onclick="window.open('https://yourgraphicdesign.com', '_blank')">Graphic Design</button>
